9b2a5a17a37708fbac415247f485b8e3c9a4289b
jacoelho <jose.coelho@linkedcare.com>
2015-08-26 17:58:52 +0100

    WIP

--- cli.go
+++ cli.go
@@ -9,11 +9,24 @@ import (
 func main() {
 	redisCluster := cluster.NewCluster([]string{"172.17.0.120:6379", "172.17.0.119:6379", "172.17.0.116:6379"})
 
-	cluster.MeetCluster(redisCluster)
+	//cluster.MeetCluster(redisCluster)
 
 	time.Sleep(5 * 1000 * time.Millisecond)
 
-	cluster.CheckCluster(redisCluster)
+	unassigned, count := cluster.CheckCluster(redisCluster)
 
-	fmt.Println(redisCluster)
+	if len(count) > 0 && len(count) < 3 {
+		panic("something is wrong")
+	}
+
+	if len(count) == 0 {
+		cluster.AssignClusterSlots(unassigned, 4)
+	}
+
+	unassigned, count = cluster.CheckCluster(redisCluster)
+
+	cluster.AssignSlaves(unassigned, count)
+
+	fmt.Println(cluster.CheckCluster(redisCluster))
+
 }
--- cluster/cluster.go
+++ cluster/cluster.go
@@ -3,8 +3,32 @@ package cluster
 import (
 	"fmt"
 	"gopkg.in/redis.v3"
+	"sort"
 	"strings"
 )
+
+// A data structure to hold key/value pairs
+type Pair struct {
+	Key   string
+	Value int
+}
+
+// A slice of pairs that implements sort.Interface to sort by values
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+
+func convertToPairList(m map[string]int) PairList {
+	result := make(PairList, len(m))
+	i := 0
+	for k, v := range m {
+		result[i] = Pair{k, v}
+		i++
+	}
+	return result
+}
 
 const CLUSTER_HASH_SLOTS = 16383
 const CLUSTER_QUORUM = 3
@@ -24,7 +48,7 @@ type ClusterNode struct {
 	pongRecv    string
 	configEpoch string
 	linkState   string
-	slot        []string
+	slots       []string
 }
 
 func parseNode(line string) *ClusterNode {
@@ -43,9 +67,46 @@ func parseNode(line string) *ClusterNode {
 		pongRecv:    fields[5],
 		configEpoch: fields[6],
 		linkState:   fields[7],
-		slot:        fields[8:],
+		slots:       fields[8:],
 	}
 	return node
+}
+
+func GenerateClusterSlots(clusterSize int) [][]int {
+	// lets slot it
+	step := CLUSTER_HASH_SLOTS / clusterSize
+	result := make([][]int, clusterSize)
+	for i := 0; i < clusterSize; i++ {
+
+		first := i * step
+		last := (i + 1) * step
+
+		if i > 0 {
+			first += 1
+		}
+
+		if (i + 1) == clusterSize {
+			last = CLUSTER_HASH_SLOTS
+		}
+
+		if first == last {
+			result[i] = []int{first}
+		} else {
+			result[i] = []int{first, last}
+		}
+	}
+
+	return result
+}
+
+// implement a binary search?
+func contains(slice []string, value string) bool {
+	for _, i := range slice {
+		if i == value {
+			return true
+		}
+	}
+	return false
 }
 
 func NewCluster(servers []string) *Cluster {
@@ -103,6 +164,22 @@ func MeetCluster(redisCluster *Cluster) error {
 	return nil
 }
 
+func AddSlave(addr string, masterId string) error {
+	client := redis.NewClient(
+		&redis.Options{
+			Addr:     addr,
+			Password: "",
+		},
+	)
+
+	err := client.ClusterReplicate(masterId).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func StopCluster(redisCluster *Cluster) error {
 	for _, client := range redisCluster.clients {
 		if err := client.Close(); err != nil {
@@ -127,10 +204,70 @@ func CheckKnownNodes(client *redis.Client) map[string]*ClusterNode {
 	return result
 }
 
-func CheckCluster(redisCluster *Cluster) error {
-	for _, client := range redisCluster.clients {
-		fmt.Println("node ->>>", CheckKnownNodes(client))
-	}
+func CheckCluster(redisCluster *Cluster) (unassigned []string, masterCount map[string]int) {
+	unassigned = make([]string, 0)
+	masterCount = make(map[string]int)
 
+	client := redisCluster.clients[0]
+	nodes := CheckKnownNodes(client)
+
+	for _, nodeInfo := range nodes {
+		// ignore failed and handshake
+		if contains(nodeInfo.flags, "handshake") || contains(nodeInfo.flags, "fail") {
+			continue
+		}
+
+		// slave node - register master
+		if nodeInfo.master != "-" {
+			masterCount[nodeInfo.master] += 1
+		} else {
+			// nodes with master status and without slots assigned
+			if len(nodeInfo.slots) == 0 {
+				unassigned = append(unassigned, nodeInfo.addr)
+			} else {
+				// nodes with master status and slots assigned
+				if _, ok := masterCount[nodeInfo.id]; !ok {
+					masterCount[nodeInfo.id] = 0
+				}
+			}
+		}
+	}
+	return
+}
+
+func AssignSlaves(unassigned []string, masterCount map[string]int) {
+	for _, item := range unassigned {
+		tmp := convertToPairList(masterCount)
+		sort.Sort(tmp)
+
+		fmt.Println("adding", item, "as slave of", tmp[0].Key)
+		AddSlave(item, tmp[0].Key)
+		masterCount[tmp[0].Key] += 1
+	}
+}
+
+func AssignClusterSlots(unassigned []string, clusterSize int) error {
+	slots := GenerateClusterSlots(clusterSize)
+
+	for idx, server := range unassigned[:clusterSize] {
+		client := redis.NewClient(
+			&redis.Options{
+				Addr:     server,
+				Password: "",
+			},
+		)
+
+		if len(slots[idx]) > 1 {
+			err := client.ClusterAddSlotsRange(slots[idx][0], slots[idx][1]).Err()
+			if err != nil {
+				return err
+			} else {
+				err := client.ClusterAddSlots(slots[idx][0]).Err()
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
 	return nil
 }
